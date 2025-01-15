82bd8193dc409072bf7ecfbec8577f4a8311fb4d
gugus7733 <augustin.aubies@gmail.com>
2025-01-16 00:08:17 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 export default function Home() {
     return (
       <div style={{ textAlign: 'center', marginTop: '20%' }}>
         <h1>Bonjour !</h1>
       </div>
     );
+=======
+export default function Home() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '20%' }}>
+        <h1>Bonjour !</h1>
+      </div>
+    );
+>>>>>>> 034ba87 (Initial commit)
   }
